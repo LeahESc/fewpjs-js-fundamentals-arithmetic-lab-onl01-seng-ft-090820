205ca79d0c068ed1a1dff2e0cc49045cb662a6d3
LeahESc <leahemily1@gmail.com>
2020-12-18 09:34:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2 
+let num2 = 31 
+
+let multiply = num1 * num2 
+
+let random = Math.ceil(Math.random * 100)
+
+let mod = 16 % 6 
+
+let max = Math.max(6, 9, 12, 20)
